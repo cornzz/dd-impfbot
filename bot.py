26519270b9f0dfce605149e29ae75d416836647f
cornzz <39997278+cornzz@users.noreply.github.com>
2021-05-12 19:09:57 +0200

    Add persistence, fix error after chat migration

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-import os
+import os, signal
 import json
 import re
+import pickle
 from datetime import datetime, timedelta
 
 import requests
-from telegram.ext import Updater, CommandHandler, JobQueue
+from telegram.ext import Updater, CommandHandler, JobQueue, Filters
 
 TOKEN = os.getenv('BOT_TOKEN')
+USER = os.getenv('TG_USER')
 URL = 'https://countee-impfee.b-cdn.net/api/1.1/de/counters/getAll/_iz_sachsen'
 INTERVAL = 180
 CITIES = ['Dresden IZ', 'Pirna IZ']
@@ -22,6 +24,7 @@ def start(update, context):
 	else:
 		log(f'Adding chat {chat_id}.')
 		CHATS.append(chat_id)
+		backup()
 		update.message.reply_text(f'Tracking locations: {", ".join(CITIES)}. Interval: {INTERVAL} seconds.')
 
 
@@ -31,15 +34,36 @@ def stop(update, context):
 	if chat_id in CHATS:
 		log(f'Removing chat {chat_id}.')
 		CHATS.remove(chat_id)
+		backup()
 		update.message.reply_text('Stopped.')
 	else:
 		update.message.reply_text('Not running.')
 
 
+def active_chats(update, context):
+	update.message.reply_text(f'Active chats: {CHATS}')
+
+
+def shutdown(update, context):
+	log('Shutdown command issued.')
+	update.message.reply_text('Shutting down...')
+	backup()
+	os.kill(os.getpid(), signal.SIGINT)
+
+
+def backup():
+	with open('chat_ids', 'wb') as chat_ids:
+		pickle.dump(CHATS, chat_ids)
+
+
 def send_message(context, message):
 	log(f'Sending message "{message.splitlines()[0]}" to chats {CHATS}')
 	for chat in CHATS:
-		context.bot.sendMessage(chat, message)
+		try:
+			context.bot.sendMessage(chat, message)
+		except:
+			log(f'Error sending message to chat {chat}, removing chat id...')
+			CHATS.remove(chat_id)
 
 
 def check(context):
@@ -69,14 +93,22 @@ def log(message):
 
 
 def main():
+	global CHATS, CITIES_AVL
 	log('Starting bot...')
+
+	if os.path.exists('chat_ids'):
+		with open('chat_ids', 'rb') as chat_ids:
+			CHATS = pickle.load(chat_ids)
+			log(f'Chats restored: {CHATS}')
 
 	updater = Updater(TOKEN)
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler('start', start))
 	dp.add_handler(CommandHandler('stop', stop))
-	
+	dp.add_handler(CommandHandler('activechats', active_chats, Filters.user(username=USER)))
+	dp.add_handler(CommandHandler('shutdown', shutdown, Filters.user(username=USER)))
+
 	jq = updater.job_queue
 	jq.run_repeating(check, interval=INTERVAL, first=10)
 
